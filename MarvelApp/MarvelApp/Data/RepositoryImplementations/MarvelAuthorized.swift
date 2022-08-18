fa1b6d4acb5c1dcc7f98ff15668761991fbb5945
Pau Blanes <pblanesp@gmail.com>
2022-08-18 14:09:03 +0200

    Improved authorization

--- MarvelApp/MarvelApp/Data/RepositoryImplementations/MarvelAuthorized.swift
+++ MarvelApp/MarvelApp/Data/RepositoryImplementations/MarvelAuthorized.swift
@@ -1,5 +1,5 @@
 //
-//  Authorized.swift
+//  MarvelAuthorized.swift
 //  MarvelApp
 //
 //  Created by Pau Blanes on 18/8/22.
@@ -10,27 +10,30 @@ import Foundation
 protocol MarvelAuthorized { }
 
 extension MarvelAuthorized {
-
     var authorization: MarvelAuthorization? {
-        let publicKey = "7ddd4dc418f8d5ace7e32bda025539a7"
-        guard let privateKey = privateKey else { return nil }
+        guard let privateKey = getKey("MARVEL_PRIVATE_KEY"),
+              let publicKey = getKey("MARVEL_PUBLIC_KEY")
+        else { return nil }
         let timestamp = "\(Date().timeIntervalSince1970)"
         let hash = MD5(string: "\(timestamp)\(privateKey)\(publicKey)")
 
         return MarvelAuthorization(ts: timestamp, apikey: publicKey, hash: hash)
     }
+}
 
-    private var privateKey: String? {
+//MARK: - Utils
+
+private extension MarvelAuthorized {
+    func getKey(_ key: String) -> String? {
         guard let filePath = Bundle.main.path(forResource: "Keys", ofType: "plist") else {
            return nil
         }
         let plist = NSDictionary(contentsOfFile: filePath)
-        guard let value = plist?.object(forKey: "MARVEL_PRIVATE_KEY") as? String else {
-            return nil
-        }
-        return value
+        return plist?.object(forKey: key) as? String
     }
 }
+
+//MARK: - Components
 
 struct MarvelAuthorization {
     let ts: String
